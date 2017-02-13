786f8ffb34de9602d5365e0ac0a42f372b9e125a
TONY-PC\Tony <skitty614@gmail.com>
2017-02-13 08:28:46 -0600

    The consequences of Inheritance

---
+++ Source/S05_TestingGrounds/Public/PatrollingGuard.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "TP_ThirdPerson/TP_ThirdPersonCharacter.h"
+#include "PatrollingGuard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class S05_TESTINGGROUNDS_API APatrollingGuard : public ATP_ThirdPersonCharacter
+{
+	GENERATED_BODY()
+	
+protected:
+	UPROPERTY(EditInstanceOnly, Category = "Patrol")
+	TArray<AActor*> PatrolPointsCPP;
+		
+};
